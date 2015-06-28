53dfc3651fe1e91513dabb663ecd081f1db15613
Admin <tremtyachiy@gmail.com>
2015-06-28 14:16:02 +0300

    add activity createprofile

---
+++ app/src/main/java/com/smithysoft/englishclub/activity/CreateProfileActivity.java
@@ -0,0 +1,29 @@
+package com.smithysoft.englishclub.activity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.smithysoft.englishclub.R;
+
+/**
+ * Created by Admin on 28.06.2015.
+ */
+public class CreateProfileActivity extends ActionBarActivity {
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.createprofilescreen);
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Create a profile");
+        toolbar.setTitleTextColor(Color.WHITE);
+        setSupportActionBar(toolbar);
+    }
+}
--- app/src/main/java/com/smithysoft/englishclub/activity/StartScreen.java
+++ app/src/main/java/com/smithysoft/englishclub/activity/StartScreen.java
@@ -1,7 +1,10 @@
 package com.smithysoft.englishclub.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.smithysoft.englishclub.R;
 
@@ -14,5 +17,15 @@ public class StartScreen extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.startscreen);
+    }
+
+    public void onCreateProfile(View view) {
+        Intent intent = new Intent(this, CreateProfileActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void onSingIn(View view) {
+        Toast.makeText(this, "Sing in", Toast.LENGTH_SHORT).show();
     }
 }
