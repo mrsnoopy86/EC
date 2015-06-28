838954ee3cbc0bfe342fcbe8762b43cad074e5ea
Admin <tremtyachiy@gmail.com>
2015-06-28 12:34:51 +0300

    add activity startscreen

---
+++ app/src/main/java/com/smithysoft/englishclub/activity/StartScreen.java
@@ -0,0 +1,18 @@
+package com.smithysoft.englishclub.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.smithysoft.englishclub.R;
+
+/**
+ * Created by Admin on 28.06.2015.
+ */
+public class StartScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.startscreen);
+    }
+}
