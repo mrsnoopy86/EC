4098ba5a0f5e4555c381b8fe4d310826b7cbad67
Admin <tremtyachiy@gmail.com>
2015-06-29 19:05:59 +0300

    add activity createprofile

--- app/src/main/java/com/smithysoft/englishclub/activity/StartScreen.java
+++ app/src/main/java/com/smithysoft/englishclub/activity/StartScreen.java
@@ -1,31 +1,80 @@
 package com.smithysoft.englishclub.activity;
 
 import android.app.Activity;
-import android.content.Intent;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.rengwuxian.materialedittext.MaterialEditText;
 import com.smithysoft.englishclub.R;
+
+import cn.pedant.SweetAlert.SweetAlertDialog;
 
 /**
  * Created by Admin on 28.06.2015.
  */
 public class StartScreen extends Activity {
+    private MaterialEditText login, password;
+    private final int DIALOG_CREATE_PROFILE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.startscreen);
+        login = (MaterialEditText) findViewById(R.id.editTextLogin);
+        password = (MaterialEditText) findViewById(R.id.editTextPass);
     }
 
     public void onCreateProfile(View view) {
-        Intent intent = new Intent(this, CreateProfileActivity.class);
-        startActivity(intent);
-        finish();
+        showDialog(DIALOG_CREATE_PROFILE);
     }
 
     public void onSingIn(View view) {
-        Toast.makeText(this, "Sing in", Toast.LENGTH_SHORT).show();
+        if(!TextUtils.isEmpty(login.getText().toString()) || !TextUtils.isEmpty(password.getText().toString())){
+            if(login.getText().toString().equals("tremtyachiy@gmail.com") && password.getText().toString().equals("tremtyachiy")){
+                Toast.makeText(this, "Sing in", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Login or password incorrect", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(this, "Input your e-mail and password", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        LinearLayout dialogView = (LinearLayout) getLayoutInflater().inflate(R.layout.alert_dialog_create_login, null);
+        dialog.setView(dialogView);
+        dialog.setCancelable(false);
+        return dialog.create();
+    }
+
+    public void onDialogButtonCreate(View view) {
+        switch (view.getId()){
+            case R.id.confirm_button_create:
+                dismissDialog(DIALOG_CREATE_PROFILE);
+                new SweetAlertDialog(this, SweetAlertDialog.SUCCESS_TYPE)
+                        .setTitleText("Successfully")
+                        .setConfirmText("OK")
+                        .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                            @Override
+                            public void onClick(SweetAlertDialog sweetAlertDialog) {
+                                sweetAlertDialog.cancel();
+                                }
+                        })
+                        .show();
+                break;
+            case R.id.cancel_button_create:
+                dismissDialog(DIALOG_CREATE_PROFILE);
+                break;
+            default:
+                break;
+
+        }
     }
 }
