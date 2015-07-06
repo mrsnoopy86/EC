987eaf7e6c3f96c21c296d15e3a45a8c0de0548c
mrsnoopy86 <tremtyachiy@gmail.com>
2015-07-06 17:01:03 +0300

    add download method

--- app/src/main/java/com/smithysoft/englishclub/adapters/CastomAdapter.java
+++ app/src/main/java/com/smithysoft/englishclub/adapters/CastomAdapter.java
@@ -1,17 +1,23 @@
 package com.smithysoft.englishclub.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.widget.ShareActionProvider;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
+import com.akexorcist.roundcornerprogressbar.IconRoundCornerProgressBar;
 import com.db.chart.listener.OnEntryClickListener;
 import com.db.chart.view.BarChartView;
 import com.db.chart.view.HorizontalBarChartView;
@@ -20,6 +26,7 @@ import com.smithysoft.englishclub.R;
 import com.smithysoft.englishclub.utils.Item;
 
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 import java.util.zip.Inflater;
 
 /**
@@ -28,7 +35,8 @@ import java.util.zip.Inflater;
 public class CastomAdapter extends BaseAdapter {
     Context context;
     ArrayList<Item> list = new ArrayList<>();
-    LayoutInflater inflater ;
+    LayoutInflater inflater;
+    Handler handler;
 
     public CastomAdapter(Context context, ArrayList<Item> list) {
         this.context = context;
@@ -45,7 +53,11 @@ public class CastomAdapter extends BaseAdapter {
         LinearLayout golosovanie;
         LinearLayout photogrid;
         LinearLayout progressBarLayout;
+        IconRoundCornerProgressBar progressBarFirst;
+        IconRoundCornerProgressBar progressBarSecond;
+        IconRoundCornerProgressBar progressBarThird;
     }
+
     @Override
     public int getCount() {
         return list.size();
@@ -63,7 +75,7 @@ public class CastomAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        Holder holder;
+        final Holder holder;
         if (convertView == null) {
             convertView = inflater.inflate(R.layout.adapter_news, parent, false);
             holder = new Holder();
@@ -74,31 +86,67 @@ public class CastomAdapter extends BaseAdapter {
             holder.golosovanie = (LinearLayout) convertView.findViewById(R.id.golosovanie);
             holder.photogrid = (LinearLayout) convertView.findViewById(R.id.photogrid);
             holder.progressBarLayout = (LinearLayout) convertView.findViewById(R.id.progressBarLayout);
+            holder.progressBarFirst = (IconRoundCornerProgressBar) convertView.findViewById(R.id.progressBarFirst);
+            holder.progressBarSecond = (IconRoundCornerProgressBar) convertView.findViewById(R.id.progressBarSecond);
+            holder.progressBarThird = (IconRoundCornerProgressBar) convertView.findViewById(R.id.progressBarThird);
             convertView.setTag(holder);
         } else {
             holder = (Holder) convertView.getTag();
         }
+        holder.progressBarFirst.setProgress(0);
+        holder.progressBarSecond.setProgress(0);
+        holder.progressBarThird.setProgress(0);
+        holder.radioGrouproup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton checkedRadioButton = (RadioButton) group.findViewById(checkedId);
+                if (checkedRadioButton.isChecked()) {
+                    holder.radioGrouproup.setVisibility(View.GONE);
+                    holder.progressBarLayout.setVisibility(View.VISIBLE);
+                    Thread thread = new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            for(int i = 0; i < 85; i++){
+                                try {
+                                    TimeUnit.MILLISECONDS.sleep(50);
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                                handler.sendEmptyMessage(i);
+                            }
+                        }
+                    });
+                    thread.start();
+                }
+            }
+        });
 
-
-        if(position%3 == 0){
+        if (position % 2 == 0) {
             holder.photogrid.setVisibility(View.VISIBLE);
             holder.golosovanie.setVisibility(View.GONE);
         } else {
-            if(position%2 == 0){
-                holder.photogrid.setVisibility(View.GONE);
-                holder.golosovanie.setVisibility(View.VISIBLE);
-                holder.radioGrouproup.setVisibility(View.VISIBLE);
-                holder.progressBarLayout.setVisibility(View.GONE);
-            } else {
-                holder.photogrid.setVisibility(View.GONE);
-                holder.golosovanie.setVisibility(View.VISIBLE);
-                holder.radioGrouproup.setVisibility(View.GONE);
-                holder.progressBarLayout.setVisibility(View.VISIBLE);
-            }
+            holder.photogrid.setVisibility(View.GONE);
+            holder.golosovanie.setVisibility(View.VISIBLE);
+            holder.radioGrouproup.setVisibility(View.VISIBLE);
+            holder.progressBarLayout.setVisibility(View.GONE);
         }
+        handler = new Handler(){
+            public void handleMessage(Message msg){
+                if(msg.what <= 10){
+                    holder.progressBarFirst.setProgress(msg.what);
+                    holder.progressBarSecond.setProgress(msg.what);
+                    holder.progressBarThird.setProgress(msg.what);
+                } else {
+                    if(msg.what <= 54){
+                        holder.progressBarSecond.setProgress(msg.what);
+                        holder.progressBarThird.setProgress(msg.what);
+                    } else {
+                        holder.progressBarSecond.setProgress(msg.what);
+                    }
+                }
+            };
+        };
 
-        return convertView;
+    return convertView;
     }
-
-
 }
